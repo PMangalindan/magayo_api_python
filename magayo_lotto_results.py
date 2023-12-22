64641fc9a13f470cb20082f0679e1b115ca0e734
PMangalindan <118822573+PMangalindan@users.noreply.github.com>
2023-12-22 17:54:14 +0800

    Add files via upload

---
+++ magayo_lotto_results.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import requests
+
+api_key = 'jhxYdhfHPKdFyhJteV' #https://www.magayo.com/api-area/account/
+game = 'ph_lotto649'           # find your game codes here https://www.magayo.com/lottery-docs/api/supported-games/
+draw_date = '2023-12-21'       # this can be blank
+
+BASE_URL = f'https://www.magayo.com/api/results.php?api_key={api_key}&game={game}&draw={draw_date}'
+
+response = requests.get(BASE_URL)
+print(response.json())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
